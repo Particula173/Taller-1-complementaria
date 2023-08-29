5fa76811d0ac0dad4d7c807b3153aa6f6bcbd268
Davidsanta0120 <davidsanta2016gmail.com>
2023-08-29 12:53:25 -0500

    1.4 terminado

--- Taller_1/indices_refraccion.py
+++ Taller_1/indices_refraccion.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def creacion_de_lista_de_tuplas(ruta:str)->list:
     archivo=open(ruta,"r")
     lista=[]
@@ -24,3 +27,30 @@ def creacion_de_lista_de_tuplas(ruta:str)->list:
 
 a="Taller_1\\Materia Inorgánica\\Bond.yml"
 print(creacion_de_lista_de_tuplas(a))
+
+
+def grafica(ruta)->None:
+    lis=creacion_de_lista_de_tuplas(ruta)
+    x=[]
+    y=[]
+    nom=nombre_material(ruta)
+    for i in lis:
+        x.append(i[0])
+        y.append(i[1])
+    x=np.array(x)
+    y=np.array(y)
+    promedio_n=round(np.mean(y),4)
+    desv_est=round(np.std(y),4)
+    plt.plot(x,y)
+    plt.title(nom+"\n n promedio:"+str(promedio_n)+" desviacion:"+str(desv_est))
+    plt.xlabel("$\lambda$")
+    plt.ylabel("indice de refraccion n")
+    plt.show()
+    
+def nombre_material(texto)->str:
+    l_arch=texto.split("\\")
+    mat=l_arch[2].split(".yml")[0]
+    
+    nom_arch=l_arch[1]+": "+mat
+    return nom_arch
+grafica(a)
